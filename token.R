9e5637ce93fd7eefb61cc7e7e8e4686af625d29a
Linn Friberg <83468574+linfri@users.noreply.github.com>
2021-08-29 13:51:15 +0200

    Pacman

--- token.R
+++ token.R
@@ -6,19 +6,22 @@
 #'
 #' @description
 #' Generates Dropbox token in the current folder.
-#' 
-#' @return 
+#'
+#' @return
 #' Returns a text message after generation.
 #'
 #'
 
-library(rdrop2)
-library(httpuv) # just in case, if rdrop2 acts up
-library(curl)
-
 generateToken <- function() {
-  if(!curl::has_internet()) stop("Please connect to the internet!")
+
+  # Initial checks
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(rdrop2, httpuv, curl)
+  if (!curl::has_internet()) stop("Please connect to the internet!")
+
+  # Token generation
   token <- drop_auth(new_user = TRUE)
   saveRDS(token, "ee977806d7286510da")
   return(print(paste0("Token saved in ", getwd())))
+  
 }
